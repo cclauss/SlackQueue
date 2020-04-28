9b3ce29ab2329d0c8eab17950a7e45ed14437db8
Christian Clauss <cclauss@me.com>
2020-04-28 16:53:53 +0200

    Python 3: import queue

--- Global.py
+++ Global.py
@@ -1,4 +1,7 @@
-import Queue as queue
+try:
+    import queue  # Python3
+except ImportError:
+    import Queue as queue  # Python 2
 
 #Instantiate a queue for the handler/listener
 slack_queue = queue.Queue(-1)
